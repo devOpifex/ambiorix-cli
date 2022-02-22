4544a0f44a3cce31ff45a48c5a3270c0e7f9c78f
John Coene <jcoenep@gmail.com>
2022-02-22 16:41:32 +0100

    fix: run app in background

--- src/app.js
+++ src/app.js
@@ -1,16 +1,28 @@
 const utils = require('./utils');
 const { spawn } = require("child_process");
+const fs = require('fs');
 
 module.exports = {
   start: () => {
     let path = utils.getFullPath('app.R');
 
-    spawn('Rscript', [path], { stdio: 'inherit' });
+    child = spawn('Rscript', [path], { stdio: 'inherit', detached: true });
 
+    child.unref();
+
+    fs.writeFile('.ambiorix.cli', child.pid.toString(), err => {
+      if(err == null)
+        return;
+
+      console.error(err);
+      child.kill('SIGKILL');
+    });
   },
   stop: () => {
     
-    spawn('R', ['-e', "ambiorix::stop_all()"], { stdio: 'inherit' });
-
+    fs.readFile('.ambiorix.cli', 'utf8', (err, data) => {
+      process.kill(data, 'SIGKILL');
+      fs.unlinkSync('.ambiorix.cli');
+    });
   }
 }
